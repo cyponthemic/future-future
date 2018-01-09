1154cb52a2a967ec673b562dd8b27f2801f7bbd7
Alex Chavet <alex@bbe.io>
2018-01-09 22:01:10 +1100

    Loading screen

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <section class="container">
+  <section class="container" :class="{'container--loading' : loading}">
+    <div class="header" v-show="!loading">
+      <ul class="nav">
+        <li><a href="">Reservation</a></li>
+        <li><a href="">Menu</a></li>
+        <li><a href="">Instagram</a></li>
+      </ul>
+    </div>
     <div>
-      <img class="main-logo" src="~/assets/images/FutureFuture_Logo_Black.svg"/>
+      <img class="main-logo" v-show="!loading" src="~/assets/images/FutureFuture_Logo_White.svg"/>
+      <img class="main-logo" v-show="loading" src="~/assets/images/FutureFuture_Logo_Black.svg"/>
+    </div>
+    <div class="footer" v-show="!loading">
+      <ul class="nav">
+        <li class="tel"><a href="">T 9812 3456</a></li>
+        <li class="address"><a href="">192 Swan Street, Richmond VIC 3121</a></li>
+        <li class="email"><a href="">Email us</a></li>
+      </ul>
     </div>
   </section>
 </template>
@@ -12,34 +27,24 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
+  },
+  data () {
+    return {
+      loading: true
+    }
+  },
+  methods: {
+    endLoading () {
+      this.loading = false
+    }
+  },
+  mounted () {
+    setTimeout(this.endLoading, 3000)
   }
 }
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  animation-name: example;
-  animation-duration: 1s;
-  animation-iteration-count: infinite;
-  animation-fill-mode: forwards;
-}
-@keyframes example {
-    from {
-      background: url(~/assets/images/bg.png);
-    }
-    to {
-      background: url(~/assets/images/bg2.png);
-    }
-    0%   { background: url(~/assets/images/bg.png) }
-     50%  { background: url(~/assets/images/bg.png) }
-     50.01%  {background: url(~/assets/images/bg2.png) }
-     100%  { background: url(~/assets/images/bg2.png) }
-}
 
 .main-logo {
   position: absolute;
@@ -49,24 +54,4 @@ export default {
   transform: translate(-50%,-50%);
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
